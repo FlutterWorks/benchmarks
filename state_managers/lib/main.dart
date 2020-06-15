a1d626346ab77869d433b8978f14c98ee1f2b51a
Jonny Borges <35742643+jonataslaw@users.noreply.github.com>
2020-06-15 12:21:40 -0300

    Added idle test

--- state_managers/lib/main.dart
+++ state_managers/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+// import 'item.dart';
 
 //import 'package:benckmark/_bloc_plain/app.dart';
 //import 'package:benckmark/_bloc_lib/app.dart';
@@ -10,3 +11,61 @@ import 'package:flutter/widgets.dart';
 import 'package:benckmark/_get/app.dart';
 
 void main() => runApp(App());
+
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Idle Sample',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Page(title: 'Idle Sample'),
+//     );
+//   }
+// }
+
+// class Page extends StatefulWidget {
+//   Page({
+//     Key key,
+//     this.title,
+//   }) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _PageState createState() => _PageState();
+// }
+
+// final items = List<Item>.of(sampleItems);
+
+// class _PageState extends State<Page> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Idle"),
+//       ),
+//       body: ListViewWidget(),
+//     );
+//   }
+// }
+
+// class ListViewWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//         padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+//         itemCount: items.length,
+//         itemBuilder: (context, index) {
+//           return ListTile(
+//             title: Text(items[index].title),
+//           );
+//         });
+//   }
+// }
